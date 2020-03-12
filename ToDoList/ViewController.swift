78efeadf4ff143a4803d54aa0dfb33e2257cf55e
Jack Boyuan Xu <boyuanxu@usc.edu>
2020-03-12 12:09:52 -0700

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Boyuan Xu on 3/12/20.
+//  Copyright © 2020 Boyuan Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
